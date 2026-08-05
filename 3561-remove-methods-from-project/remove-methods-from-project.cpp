857d3add3a7a91cb4d9290f0308bf48084292ab9
Khushehehe <kapoorkhushi829@gmail.com>
2026-08-05 23:15:51 +0530

    Time: 795 ms (5.86%) | Memory: 324.4 MB (49.66%) - LeetSync

---
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
+            vector<vector<int>> graph(n);
+
+        for (auto &e : invocations)
+            graph[e[0]].push_back(e[1]);
+
+        vector<bool> suspicious(n, false);
+
+        function<void(int)> dfs = [&](int u) {
+            suspicious[u] = true;
+            for (int v : graph[u]) {
+                if (!suspicious[v])
+                    dfs(v);
+            }
+        };
+
+        // Mark all suspicious methods
+        dfs(k);
+
+        // If any outside method calls a suspicious method,
+        // removal is impossible.
+        for (auto &e : invocations) {
+            int u = e[0], v = e[1];
+            if (!suspicious[u] && suspicious[v]) {
+                vector<int> ans;
+                for (int i = 0; i < n; i++)
+                    ans.push_back(i);
+                return ans;
+            }
+        }
+
+        // Return remaining methods
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i])
+                ans.push_back(i);
+        }
+
+        return ans;
+
+    }
+};
