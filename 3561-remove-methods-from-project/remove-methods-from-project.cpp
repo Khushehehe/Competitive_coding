9d3942800b9e22df013385495107ef02471f41c2
Khushehehe <kapoorkhushi829@gmail.com>
2026-08-06 19:07:55 +0530

    Time: 175 ms (67.01%) | Memory: 301.2 MB (80.61%) - LeetSync

--- 3561-remove-methods-from-project/remove-methods-from-project.cpp
+++ 3561-remove-methods-from-project/remove-methods-from-project.cpp
@@ -1,44 +1,41 @@
 class Solution {
 public:
     vector<int> remainingMethods(int n, int k, vector<vector<int>>& invocations) {
-            vector<vector<int>> graph(n);
-
-        for (auto &e : invocations)
-            graph[e[0]].push_back(e[1]);
-
-        vector<bool> suspicious(n, false);
-
-        function<void(int)> dfs = [&](int u) {
-            suspicious[u] = true;
-            for (int v : graph[u]) {
-                if (!suspicious[v])
-                    dfs(v);
-            }
-        };
-
-        // Mark all suspicious methods
-        dfs(k);
-
-        // If any outside method calls a suspicious method,
-        // removal is impossible.
-        for (auto &e : invocations) {
-            int u = e[0], v = e[1];
-            if (!suspicious[u] && suspicious[v]) {
-                vector<int> ans;
-                for (int i = 0; i < n; i++)
-                    ans.push_back(i);
-                return ans;
-            }
-        }
-
-        // Return remaining methods
         vector<int> ans;
-        for (int i = 0; i < n; i++) {
-            if (!suspicious[i])
-                ans.push_back(i);
+        vector<vector<int>> graph(n);
+        for(auto &u: invocations){
+            graph[u[0]].push_back(u[1]);
         }
-
+        vector<int> visited(n, false);
+        queue<int> q;
+        q.push({k});
+        visited[k]=true;
+        while(q.size()>0){
+            int u=q.front();
+            q.pop();
+            for(auto v: graph[u]){
+                if(!visited[v]){
+                    q.push(v);
+                    visited[v]=true;
+                }
+            }
+        }
+        for(int i=0;i<invocations.size();i++){
+            if(!visited[invocations[i][0]] && visited[invocations[i][1]]){
+                for(int i=0;i<n;i++){
+                    visited[i]=false;
+                }
+                break;
+            }
+            if(!visited[invocations[i][1]]){
+                visited[invocations[i][1]]=false;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                ans.push_back(i);
+            }
+        }
         return ans;
-
     }
 };
