d6d4454b8ed5310a30f6e8392d6861c333eed54f
Khushehehe <kapoorkhushi829@gmail.com>
2026-09-09 18:42:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (84.80%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans=0;
+        long long k=999; 
+        while(n>k){
+            ans+=n-k;
+            k= k*1000+ 999;
+        }
+        return ans;                                                               
+    }
+};
